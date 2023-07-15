a7376c8aa047073ed7608a9f084f817a547adf7e
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-16 00:28:26 +0100

    fix: updated way of creating db

--- database.js
+++ database.js
@@ -1,46 +1,62 @@
-const { createEngine, Column } = require('typeorm');
-
-// Database connection
-const engine = createEngine({
-    type: 'sqlite',
-    database: 'quotes.db',
-    synchronize: true,
-    entities: [Quote],
-});
+const { createConnection, EntitySchema, PrimaryGeneratedColumn, Column } = require('typeorm');
 
 // Defines table for quotes
 class Quote {
-    @Column({ primary: true, generated: 'increment' })
-    id;
-
-    @Column()
-    author;
-
-    @Column()
-    content;
+    constructor() {
+        this.id = undefined;
+        this.author = undefined;
+        this.content = undefined;
+    }
 }
+
+// Create entity schema for Quote
+const QuoteSchema = new EntitySchema({
+    name: 'Quote',
+    target: Quote,
+    columns: {
+        id: {
+            primary: true,
+            generated: 'increment',
+            type: Number,
+        },
+        author: {
+            type: String,
+        },
+        content: {
+            type: String,
+        },
+    },
+});
+
+// Database connection
+const engine = createConnection({
+    type: 'sqlite',
+    database: 'quotes.db',
+    synchronize: true,
+    entities: [QuoteSchema],
+});
 
 // Saves quotes to db
 async function saveQuote(quote) {
-    const connection = await engine.createConnection();
+    const connection = await engine;
     await connection.manager.save(quote);
 }
 
 // Deletes single quote
 async function deleteQuote(quote) {
-    const connection = await engine.createConnection();
+    const connection = await engine;
     await connection.manager.delete(quote);
 }
 
 // Deletes all quotes
 async function deleteAllQuotes() {
-    const connection = await engine.createConnection();
+    const connection = await engine;
     await connection.manager.delete(Quote);
 }
 
 // Creates table if it doesn't already exist
 async function createQuotesTable() {
-    const connection = await engine.createConnection();
+    const connection = await engine;
     const hasTable = await connection.query(
         "SELECT name FROM sqlite_master WHERE type='table' AND name='quotes';"
     );
