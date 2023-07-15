9f8f7a044da83ccdd73d477091485ebd7f5998aa
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-16 00:28:58 +0100

    feat: created client

--- bot.js
+++ bot.js
@@ -1,6 +1,21 @@
-const {
-    saveQuote,
-    deleteQuote,
-    deleteAllQuotes,
-    createQuotesTable
-} = require('./quotes');
+// Discord.js
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Database functions
+const { saveQuote, deleteQuote, deleteAllQuotes, createQuotesTable } = require('./database.js');
+
+// Config
+// Token
+require('dotenv').config();
+const token = process.env.DISCORD_TOKEN;
+
+// Create new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Runs only once when client is ready
+client.once(Events.ClientReady, c => {
+	console.log(`${c.user.tag} is now online!`);
+});
+
+// Log in w/ client token
+client.login(token)
