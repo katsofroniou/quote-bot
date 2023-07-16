f2aa15247a35bd3ef237ffe086d08db8dbc2a9f5
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-16 04:03:36 +0100

    feat: grabs username and message from link correctly

---
+++ commands/quoting/quoteLink.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { saveQuote } = require("../../database.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('quotelink')
+		.setDescription('Eternalise a message')
+		.addStringOption(option =>
+			option
+				.setName('link')
+				.setDescription('Link to the message you want to quote')
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		const link = interaction.options.getString('link');
+
+		const splitLink = link.split('/');
+		const guildId = splitLink[4];
+		const channelId = splitLink[5];
+		const messageId = splitLink[6];
+
+		try {
+			// Fetching the message based on the link
+			const guild = interaction.client.guilds.cache.get(guildId);
+			const channel = guild.channels.cache.get(channelId);
+			const message = await channel.messages.fetch(messageId);
+
+			// Grabs message and username of who sent it
+			const content = message.content;
+			const author = message.author;
+			const username = `${author.username}#${author.discriminator}`
+
+			// Debug
+			console.log(`message:${content}   author:${username}`);
+		} catch (error) {
+			console.error('Error retrieving the message:', error);
+		}
+	}
+}
