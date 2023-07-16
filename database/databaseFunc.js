2d1e23e9e42fdafe6c0b7bca6897e0114a999c6a
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-17 00:07:12 +0100

    feat: created functions for adding quote / creating table

--- database/databaseFunc.js
+++ database/databaseFunc.js
@@ -1,31 +1,33 @@
-async function createTable(db) {
-	try {
-		await db.run(`
-        CREATE TABLE IF NOT EXISTS quotes (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                author TEXT
-                content TEXT
-        )`);
-
-		console.log('Table created/already exists');
-	}
-	catch (err) {
-		console.log(`Error occured: ${err}`);
-	}
+function createTable(db) {
+	db.serialize(() =>
+		db.run(`CREATE TABLE IF NOT EXISTS quote (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			author TEXT,
+			content TEXT
+		);`),
+	);
 }
 
-async function saveQuote(db, author, content) {
-	try {
-		await db.run(`
-        INSERT INTO quotes (author, content)
-        VALUES (?, ?);`, [author, content]);
-	}
-	catch (err) {
-		console.log(`${err}`);
-	}
+function addQuote(db, author, quote) {
+	db.serialize(() =>
+		db.run(`INSERT INTO quotes (author, content)
+				VALUES (?, ?);`,
+		[author, quote],
+		(err) => {
+			console.error(err.message);
+		}),
+	);
 }
 
 module.exports = {
 	createTable,
-	saveQuote,
+	addQuote,
+	getQuote,
+	getAllQuotes,
+	deleteQuote,
+	deleteAllQuotes,
 };
+
+// db.serialize(() =>
+// 	db.run(``),
+// );
