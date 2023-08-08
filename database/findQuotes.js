824b59c3048c4880b1a522d82c972ac0a9df18d5
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-08-08 21:53:03 +0100

    fix: got author, not creator, using regex for partial matches

--- database/findQuotes.js
+++ database/findQuotes.js
@@ -1,12 +1,13 @@
 const { dbClient } = require('./dbFunc');
 
 // By author, id, all
-async function findQuoteByAuthor(guildId, creator) {
+async function findQuoteByAuthor(guildId, author) {
 	try {
 		const database = dbClient.db(guildId);
 		const quotesColl = database.collection('quotes');
 
-		return await quotesColl.findOne({ creator: creator });
+		const regex = new RegExp(author, 'i');
+		return await quotesColl.find({ author: regex });
 	}
 	catch (error) {
 		console.log('Error finding quote: ', error);
