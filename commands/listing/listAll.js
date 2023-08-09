9e81a5bc09b60c759935bfda5842cad23881232b
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-08-09 23:52:26 +0100

    feat: updated embed + command permissions

--- commands/listing/listAll.js
+++ commands/listing/listAll.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Colors } = require('discord.js');
 const { findAllQuotes } = require('../../database/findQuotes');
+const { noQuoteError, errorEmbed, notYourList, permissionErrorEmbed } = require('../../embeds');
+const { checkPerms } = require('../../checkPerms');
 
 module.exports = {
 	name: 'listall',
@@ -8,80 +10,98 @@ module.exports = {
 		.setDescription('List all quotes, don\'t blame me if you\'re scarred'),
 
 	async execute(interaction) {
-		// Your permission check code here
-
-		const guildId = interaction.guildId;
-		const quotesArray = await findAllQuotes(guildId);
-
-		const itemsPerPage = 15;
-		const pageCount = Math.ceil(quotesArray.length / itemsPerPage);
-
-		let currentPage = 1;
-		let startIndex = (currentPage - 1) * itemsPerPage;
-		let endIndex = currentPage * itemsPerPage;
-		let currentQuotes = quotesArray.slice(startIndex, endIndex);
-
-		const formattedQuotesList = currentQuotes.map((quote, index) => {
-			const displayIndex = startIndex + index + 1;
-			return `**${displayIndex}**) ${quote.content} - ${quote.author}`;
-		}).join('\n');
-
-
-		const embed = new EmbedBuilder()
-			.setColor(Colors.Orange)
-			.setTitle('Quotes')
-			.setDescription(formattedQuotesList);
-
-		const prevButton = new ButtonBuilder()
-			.setCustomId('prev')
-			.setLabel('Previous')
-			.setStyle(ButtonStyle.Secondary)
-			.setDisabled(currentPage === 1);
-
-		const nextButton = new ButtonBuilder()
-			.setCustomId('next')
-			.setLabel('Next')
-			.setStyle(ButtonStyle.Primary)
-			.setDisabled(currentPage === pageCount);
-
-		const buttonRow = new ActionRowBuilder()
-			.addComponents(prevButton, nextButton);
-
-		const reply = await interaction.reply({ embeds: [embed], components: [buttonRow], fetchReply: true });
-
-		const filter = i => i.customId === 'prev' || i.customId === 'next';
-		const collector = reply.createMessageComponentCollector({ filter, time: 60000 });
-
-		collector.on('collect', async i => {
-			if (i.customId === 'prev' && currentPage > 1) {
-				currentPage--;
-			}
-			else if (i.customId === 'next' && currentPage < pageCount) {
-				currentPage++;
+		try {
+			if (!checkPerms(interaction)) {
+				return interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
 			}
 
-			startIndex = (currentPage - 1) * itemsPerPage;
-			endIndex = currentPage * itemsPerPage;
-			currentQuotes = quotesArray.slice(startIndex, endIndex);
+			const guildId = interaction.guildId;
+			const quotesArray = await findAllQuotes(guildId);
 
-			const updatedFormattedQuotesList = currentQuotes.map((quote, index) => {
+			if (quotesArray.length === 0) {
+				await interaction.reply({ embeds: [noQuoteError], ephemeral:true });
+				return;
+			}
+
+			const itemsPerPage = 15;
+			const pageCount = Math.ceil(quotesArray.length / itemsPerPage);
+
+			let currentPage = 1;
+			let startIndex = (currentPage - 1) * itemsPerPage;
+			let endIndex = currentPage * itemsPerPage;
+			let currentQuotes = quotesArray.slice(startIndex, endIndex);
+
+			const formattedQuotesList = currentQuotes.map((quote, index) => {
 				const displayIndex = startIndex + index + 1;
-				return `**${displayIndex}**) ${quote.content} - ${quote.author}`;
+				return `**${displayIndex}**) "${quote.content}" - ${quote.author}`;
 			}).join('\n');
 
-			embed.setDescription(updatedFormattedQuotesList);
+			const embed = new EmbedBuilder()
+				.setColor(Colors.Orange)
+				.setTitle(`:scroll: Quotes - Page ${currentPage}/${pageCount}`)
+				.setDescription(formattedQuotesList);
 
-			prevButton.setDisabled(currentPage === 1);
-			nextButton.setDisabled(currentPage === pageCount);
+			const prevButton = new ButtonBuilder()
+				.setCustomId('prev')
+				.setLabel('Previous')
+				.setStyle(ButtonStyle.Secondary)
+				.setDisabled(currentPage === 1);
 
-			await i.update({ embeds: [embed], components: [buttonRow] });
-		});
+			const nextButton = new ButtonBuilder()
+				.setCustomId('next')
+				.setLabel('Next')
+				.setStyle(ButtonStyle.Primary)
+				.setDisabled(currentPage === pageCount);
 
-		collector.on('end', collected => {
-			if (collected.size === 0) {
-				buttonRow.components.forEach(component => component.setDisabled(true));
-				reply.edit({ components: [buttonRow] });
-			}
-		});
+			const buttonRow = new ActionRowBuilder()
+				.addComponents(prevButton, nextButton);
+
+			const reply = await interaction.reply({ embeds: [embed], components: [buttonRow], fetchReply: true });
+
+			const filter = i => i.customId === 'prev' || i.customId === 'next';
+			const collector = reply.createMessageComponentCollector({ filter, time: 60000 });
+
+			collector.on('collect', async i => {
+				if (i.user.id !== interaction.user.id) {
+					await i.reply({ embeds: [notYourList], ephemeral: true });
+					return;
+				}
+
+				if (i.customId === 'prev' && currentPage > 1) {
+					currentPage--;
+				}
+				else if (i.customId === 'next' && currentPage < pageCount) {
+					currentPage++;
+				}
+
+				startIndex = (currentPage - 1) * itemsPerPage;
+				endIndex = currentPage * itemsPerPage;
+				currentQuotes = quotesArray.slice(startIndex, endIndex);
+
+				const updatedFormattedQuotesList = currentQuotes.map((quote, index) => {
+					const displayIndex = startIndex + index + 1;
+					return `**${displayIndex}**) "${quote.content}" - ${quote.author}`;
+				}).join('\n');
+
+				embed.setTitle(`:scroll: Quotes - Page ${currentPage}/${pageCount}`);
+				embed.setDescription(updatedFormattedQuotesList);
+
+				prevButton.setDisabled(currentPage === 1);
+				nextButton.setDisabled(currentPage === pageCount);
+
+				await i.update({ embeds: [embed], components: [buttonRow] });
+			});
+
+			collector.on('end', collected => {
+				if (collected.size === 0) {
+					buttonRow.components.forEach(component => component.setDisabled(true));
+					reply.edit({ components: [buttonRow] });
+				}
+			});
+		}
+		catch (error) {
+			console.log(error);
+			await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+		}
 	},
 };
