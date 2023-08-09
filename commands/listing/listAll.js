52130be148d7599c880572d32e147bbe9d12a710
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-08-09 23:28:17 +0100

    feat: formatted embed to display quote content then author

--- commands/listing/listAll.js
+++ commands/listing/listAll.js
@@ -1,7 +1,5 @@
-const { SlashCommandBuilder, Embed, EmbedBuilder, Colors } = require('discord.js');
-const { checkPerms } = require('../../checkPerms');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Colors } = require('discord.js');
 const { findAllQuotes } = require('../../database/findQuotes');
-
 
 module.exports = {
 	name: 'listall',
@@ -10,23 +8,80 @@ module.exports = {
 		.setDescription('List all quotes, don\'t blame me if you\'re scarred'),
 
 	async execute(interaction) {
-		if (!checkPerms(interaction)) {
-			return interaction.reply('You do not have permission to use this command');
-		}
+		// Your permission check code here
 
 		const guildId = interaction.guildId;
 		const quotesArray = await findAllQuotes(guildId);
-		console.log(quotesArray);
 
-		const quotesList = quotesArray.map(quote => {
-			return `_id: ${quote._id}, author: ${quote.author}, content: ${quote.content}, channel_id: ${quote.channel_id}, creator: ${quote.creator}`;
+		const itemsPerPage = 15;
+		const pageCount = Math.ceil(quotesArray.length / itemsPerPage);
+
+		let currentPage = 1;
+		let startIndex = (currentPage - 1) * itemsPerPage;
+		let endIndex = currentPage * itemsPerPage;
+		let currentQuotes = quotesArray.slice(startIndex, endIndex);
+
+		const formattedQuotesList = currentQuotes.map((quote, index) => {
+			const displayIndex = startIndex + index + 1;
+			return `**${displayIndex}**) ${quote.content} - ${quote.author}`;
 		}).join('\n');
+
 
 		const embed = new EmbedBuilder()
 			.setColor(Colors.Orange)
 			.setTitle('Quotes')
-			.setDescription(`${quotesList}`);
+			.setDescription(formattedQuotesList);
 
-		await interaction.reply({ embeds: [embed], ephemeral: false });
+		const prevButton = new ButtonBuilder()
+			.setCustomId('prev')
+			.setLabel('Previous')
+			.setStyle(ButtonStyle.Secondary)
+			.setDisabled(currentPage === 1);
+
+		const nextButton = new ButtonBuilder()
+			.setCustomId('next')
+			.setLabel('Next')
+			.setStyle(ButtonStyle.Primary)
+			.setDisabled(currentPage === pageCount);
+
+		const buttonRow = new ActionRowBuilder()
+			.addComponents(prevButton, nextButton);
+
+		const reply = await interaction.reply({ embeds: [embed], components: [buttonRow], fetchReply: true });
+
+		const filter = i => i.customId === 'prev' || i.customId === 'next';
+		const collector = reply.createMessageComponentCollector({ filter, time: 60000 });
+
+		collector.on('collect', async i => {
+			if (i.customId === 'prev' && currentPage > 1) {
+				currentPage--;
+			}
+			else if (i.customId === 'next' && currentPage < pageCount) {
+				currentPage++;
+			}
+
+			startIndex = (currentPage - 1) * itemsPerPage;
+			endIndex = currentPage * itemsPerPage;
+			currentQuotes = quotesArray.slice(startIndex, endIndex);
+
+			const updatedFormattedQuotesList = currentQuotes.map((quote, index) => {
+				const displayIndex = startIndex + index + 1;
+				return `**${displayIndex}**) ${quote.content} - ${quote.author}`;
+			}).join('\n');
+
+			embed.setDescription(updatedFormattedQuotesList);
+
+			prevButton.setDisabled(currentPage === 1);
+			nextButton.setDisabled(currentPage === pageCount);
+
+			await i.update({ embeds: [embed], components: [buttonRow] });
+		});
+
+		collector.on('end', collected => {
+			if (collected.size === 0) {
+				buttonRow.components.forEach(component => component.setDisabled(true));
+				reply.edit({ components: [buttonRow] });
+			}
+		});
 	},
 };
