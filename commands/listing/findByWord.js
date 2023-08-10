f21c35c834eced6b4becb4d6a52f35225396c521
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-08-10 22:56:41 +0100

    feat: added command to search by phrase in quote

---
+++ commands/listing/findByWord.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { findQuoteByPhrase } = require('../../database/findQuotes');
+const { errorEmbed, noQuoteWordError, multiQuoteFind } = require('../../embeds');
+
+
+module.exports = {
+	name: 'searchphrase',
+	data: new SlashCommandBuilder()
+		.setName('searchphrase')
+		.setDescription('Search for quotes containing a certain phrase')
+		.addStringOption(option =>
+			option
+				.setName('word')
+				.setDescription('What phrase do you want to find?')
+				.setRequired(true),
+		),
+
+	async execute(interaction) {
+		const guildId = interaction.guildId;
+
+		const wordToFind = interaction.options.getString('word');
+		const quotes = await findQuoteByPhrase(guildId, wordToFind);
+
+		try {
+
+			console.log(quotes);
+			if (quotes.length > 0) {
+				const formattedQuotesList = quotes.map((quote, index) => {
+					return `**${index + 1}**) "${quote.content}" - ${quote.author}`;
+				}).join('\n');
+
+				console.log(quotes);
+				await interaction.reply({ embeds: [multiQuoteFind(wordToFind, formattedQuotesList)] });
+			}
+			else {
+				interaction.reply({ embeds: [noQuoteWordError(wordToFind)], ephemeral: false });
+			}
+		}
+		catch (error) {
+			await interaction.reply({ embed: [errorEmbed], ephemeral: true });
+			console.log(error);
+		}
+	},
+};
